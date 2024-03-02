2c2336568144d0846bfdadcfd505c65572c4a948
Vansh Chavda <vanshchavda111@gmail.com>
2024-03-02 22:36:37 +0530

    Fix CORS

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin:process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
--- data/data.js
+++ data/data.js
@@ -2,11 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.dbURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    await mongoose.connect(process.env.dbURI).then(() => {
+      console.log("MongoDB connected");
     });
-    console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
     process.exit(1); // Exit process with failure
